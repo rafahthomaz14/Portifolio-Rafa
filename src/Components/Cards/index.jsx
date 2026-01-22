667806b3282d7c03e75b2379530be69ca79b4248
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2026-01-22 17:13:43 -0300

    adicionando novo projeto

---
+++ src/Components/Cards/index.jsx
@@ -0,0 +1,64 @@
+export default function Cards() {
+    return (
+        <div className="max-w-[1366px] px-6 mx-auto">
+            <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+
+                <div className="">
+                    <img src="Foto.jpeg" className="w-full h-40 object-cover"/>
+                    <h1 className="font-bold text-gray-50 mt-3">Software de Cantina</h1>
+                    <p className="font-extralight text-gray-200 mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+
+                <div className="">
+                    <img src="Foto.jpeg" className="w-full h-40 object-cover"/>
+                    <h1 className="font-bold text-gray-50 mt-3">Software de Cantina</h1>
+                    <p className="font-extralight text-gray-200 mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+
+                <div className="">
+                    <img src="Foto.jpeg" className="w-full h-40 object-cover"/>
+                    <h1 className="font-bold text-gray-50 mt-3">Software de Cantina</h1>
+                    <p className="font-extralight text-gray-200 mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+
+                <div className="">
+                    <img src="Foto.jpeg" className="w-full h-40 object-cover"/>
+                    <h1 className="font-bold text-gray-50 mt-3">Software de Cantina</h1>
+                    <p className="font-extralight text-gray-200 mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+
+                <div className="">
+                    <img src="Foto.jpeg" className="w-full h-40 object-cover"/>
+                    <h1 className="font-bold text-gray-50 mt-3">Software de Cantina</h1>
+                    <p className="font-extralight text-gray-200 mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+
+                <div className="">
+                    <img src="Foto.jpeg" className="w-full h-40 object-cover"/>
+                    <h1 className="font-bold text-gray-50 mt-3">Software de Cantina</h1>
+                    <p className="font-extralight text-gray-200 mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+
+                <div className="">
+                    <img src="Foto.jpeg" className="w-full h-40 object-cover"/>
+                    <h1 className="font-bold text-gray-50 mt-3">Software de Cantina</h1>
+                    <p className="font-extralight text-gray-200 mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+            </section>
+        </div>
+    )
+}
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,67 @@
+import { FaLinkedinIn, FaGithub, FaWhatsapp, FaEnvelope } from "react-icons/fa";
+import Cards from "../../Components/Cards";
+export default function Home() {
+    return (
+        <>
+            <div className="grid grid-cols-1 lg:grid-cols-[360px_1fr] gap-5 lg:gap-10 w-full max-w-[1366px] px-6 mx-auto mt-25 mb-[40px]">
+
+                <section className="relative bg-gray-50 rounded-lg pt-24">
+                    <picture className="absolute -top-20 left-1/2 -translate-x-1/2">
+                        <img
+                            src="foto.jpeg"
+                            className="w-40 h-40 rounded-full border-4 border-white object-cover"
+                        />
+                    </picture>
+                    <div className="flex flex-col justify-center items-center">
+                        <h1 className="font-bold text-2xl">Rafael Thomaz</h1>
+                        <p className="font-light text-gray-500">Desenvolvedor Front End</p>
+                    </div>
+                    <div className="grid grid-cols-4 gap-4 mt-6 px-6 mb-5">
+
+                        <a
+                            href="#"
+                            target="_blank"
+                            className="h-10 flex items-center justify-center bg-gray-200 rounded-lg hover:bg-blue-600 hover:text-white transition"
+                        >
+                            <FaLinkedinIn size={22} />
+                        </a>
+
+                        <a
+                            href="#"
+                            target="_blank"
+                            className="h-10 flex items-center justify-center bg-gray-200 rounded-lg hover:bg-gray-900 hover:text-white transition"
+                        >
+                            <FaGithub size={22} />
+                        </a>
+
+                        <a
+                            href="https://wa.me/5516993696126"
+                            target="_blank"
+                            className="h-10 flex items-center justify-center bg-gray-200 rounded-lg hover:bg-green-500 hover:text-white transition"
+                        >
+                            <FaWhatsapp size={22} />
+                        </a>
+
+                        <a
+                            href="#"
+                            className="h-10 flex items-center justify-center bg-gray-200 rounded-lg hover:bg-red-500 hover:text-white transition"
+                        >
+                            <FaEnvelope size={22} />
+                        </a>
+
+                    </div>
+                </section>
+
+                <section className="bg-gray-50 rounded-lg lg:h-60 p-5">
+                    <h1 className="font-bold text-2xl">Sobre</h1>
+                    <p className="font-extralight mt-3 text-[20px]">lorem Lorem ipsum, dolor sit amet  Lorem ipsum, dolor sit amet consectetur a consectetur adipisicing elit. Quae perferendis magnam neque provident reiciendis. Et numquam dolores tempore sunt, iste culpa ducimus fugit excepturi iusto modi aspernatur, expedita distinctio! Doloremque!</p>
+                </section>
+            </div>
+
+            <h1 className="font-bold text-2xl text-gray-50 w-full max-w-[1366px] mb-5 px-6 mx-auto flex items-center justify-center lg:justify-start ">Meus projetos</h1>
+            <Cards />
+        </>
+
+
+    )
+}
