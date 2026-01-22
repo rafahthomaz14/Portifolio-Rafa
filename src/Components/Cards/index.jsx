25d16b7afa1a065a57da3e71783d5c27d34a51a1
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2026-01-22 20:17:03 -0300

    style: ajuste

--- src/Components/Cards/index.jsx
+++ src/Components/Cards/index.jsx
@@ -6,29 +6,29 @@ export default function Cards() {
                 <div className="Mre">
                     <img src="mre.jpeg" className="w-full lg:h-30  " />
                     <h1 className="font-bold text-gray-50 mt-3">Mre Soluções</h1>
-                    <p className="font-extralight text-gray-200 mt-2 mb-2">
+                    <p className="font-extralight text-gray-200 mt-2 ">
                         Landing Page personalizada.
                     </p>
-                    <p className="font-extralight text-gray-500 "> TypeScript | JavaScript | Css</p>
+                    <p className="font-extralight text-gray-500 mb-2 "> TypeScript | JavaScript | Css</p>
                 </div>
 
                 <div className="Vila">
                     <img src="vila.jpeg" className="w-full lg:h-30 " />
                     <h1 className="font-bold text-gray-50 mt-3">Villa Pomar Hortifruti</h1>
-                    <p className="font-extralight text-gray-200 mt-2 mb-2">
+                    <p className="font-extralight text-gray-200 mt-2 ">
                         Landing Page personalizada.
                     </p>
-                    <p className="font-extralight text-gray-500 ">Html | Css | JavaScript</p>
+                    <p className="font-extralight text-gray-500 mb-2">Html | Css | JavaScript</p>
 
                 </div>
 
                 <div className="Plantech">
                     <img src="plantech.jpg" className="w-full lg:h-30 " />
                     <h1 className="font-bold text-gray-50 mt-3">Plantech</h1>
-                    <p className="font-extralight text-gray-200 mt-2 mb-2">
+                    <p className="font-extralight text-gray-200 mt-2 ">
                         Software de gerenciamento de fazendas urbanas .
                     </p>
-                    <p className="font-extralight text-gray-500 ">C# | SqLite | JavaScript</p>
+                    <p className="font-extralight text-gray-500 mb-2">C# | SqLite | JavaScript</p>
                 </div>
 
 
